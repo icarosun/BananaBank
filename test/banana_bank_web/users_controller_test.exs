aec457422a24e83d3e57a858a4590e66e39a5324
Ícarosun <icaropereirasun@gmail.com>
2026-07-06 18:49:36 -0400

    create delete test

--- test/banana_bank_web/users_controller_test.exs
+++ test/banana_bank_web/users_controller_test.exs
@@ -1,6 +1,9 @@
 defmodule BananaBankWeb.UsersControllerTest do
   use BananaBankWeb.ConnCase
 
+  alias BananaBank.Users
+  alias Users.User
+
   describe "create/2" do
     test "successfully creates an user", %{conn: conn} do
       params = %{
@@ -19,6 +22,54 @@ defmodule BananaBankWeb.UsersControllerTest do
                "data" => %{"cep" => "12345678", "email" => "joao@frutas.com", "id" => _id, "name" => "Joao"},
                "message" => "User criado com sucesso!"
              } = response
+    end
+
+    test "when there are invalid params, returns an error", %{conn: conn} do
+      params = %{
+        name: nil,
+        cep: "12",
+        password: nil,
+        email: "joao@frutas.com"
+      }
+
+      response =
+        conn
+        |> post(~p"/api/users", params)
+        |> json_response(:bad_request)
+
+      expected_response = %{
+        "errors" => %{
+          "cep" => ["should be 8 character(s)"],
+          "name" => ["can't be blank"],
+          "password" => ["can't be blank"]
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "delete/2" do
+    test "successfully deletes an user", %{conn: conn} do
+      params = %{
+        name: "Joao",
+        cep: "12345678",
+        password: "123456",
+        email: "joao@frutas.com"
+      }
+
+      {:ok, %User{id: id}} = Users.create(params)
+
+      response =
+        conn
+        |> delete(~p"/api/users/#{id}")
+        |> json_response(:ok)
+
+      expected_response = %{
+        "data" => %{"cep" => "12345678", "email" => "joao@frutas.com", "id" => id, "name" => "Joao"}
+      }
+
+      assert response == expected_response
     end
   end
 end
